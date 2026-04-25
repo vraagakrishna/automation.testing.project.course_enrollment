3587080dfa32bb4f0df1efd9b68c98aec1394de1
Raaga Krishna Velthapu <vraagakrishna@gmail.com>
2026-04-25 18:27:37 +0200

    Fixed test

--- src/pages/home-page.ts
+++ src/pages/home-page.ts
@@ -14,7 +14,7 @@ export class HomePage extends BasePage {
 
         await expect(this.page).toHaveTitle(this.homePageTitle);
 
-        const expectedHeading = 'Master Test Automation2';
+        const expectedHeading = 'Master Test Automation';
 
         await this.containsText(this.homePageHeadingLocator, expectedHeading);
     }
