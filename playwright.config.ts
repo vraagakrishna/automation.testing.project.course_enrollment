c913c25c5df49944e2827563d81f51bda2a94439
Raaga Krishna Velthapu <vraagakrishna@gmail.com>
2026-04-26 18:45:15 +0200

    Increasing global timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,6 +34,9 @@ export default defineConfig({
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: 'html',
 
+    /* Timeout for each test in milliseconds */
+    timeout: 60_000, // 1 minute per test
+
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Whether to run browser in headless mod */
@@ -50,6 +53,17 @@ export default defineConfig({
 
         /* Whether to record video for each test */
         video: 'retain-on-failure',
+
+        /* Default timeout for each Playwright action in milliseconds */
+        actionTimeout: 30_000, // 30 seconds
+
+        /* Timeout for each navigation action in milliseconds */
+        navigationTimeout: 30_000, // 30 seconds
+    },
+
+    expect: {
+        /* Default timeout for async expect matchers in milliseconds */
+        timeout: 10_000, // 10 seconds
     },
 
     /* Configure projects for major browsers */
