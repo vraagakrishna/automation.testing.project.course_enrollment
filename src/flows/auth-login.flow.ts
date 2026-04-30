ac08a1b8d912390dedac97bd5742240f64aea580
Raaga Krishna Velthapu <vraagakrishna@gmail.com>
2026-04-30 07:44:04 +0200

    Using class name instead of any - auth-login flow

--- src/flows/auth-login.flow.ts
+++ src/flows/auth-login.flow.ts
@@ -1,6 +1,17 @@
+import { LoginPage } from '../pages/auth/login-page';
+import { DashboardPage } from '../pages/dashboard/dashboard-page';
 import { env } from '../utils/env';
 
-export async function loginAndVerify(loginAs: 'user' | 'admin', { loginReady, dashboardPage }: any) {
+export async function loginAndVerify(
+    loginAs: 'user' | 'admin',
+    {
+        loginReady,
+        dashboardPage,
+    }: {
+        loginReady: LoginPage;
+        dashboardPage: DashboardPage;
+    }
+) {
     console.log(`Login as ${loginAs} and verify...`);
     if (loginAs === 'user') await loginReady.loginUser(env.userEmail, env.userPassword);
     else await loginReady.loginUser(env.adminEmail, env.adminPassword);
